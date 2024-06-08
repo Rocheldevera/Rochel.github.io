1a2a6f2e64297b565d642f19270456ded879d304
Rocheldevera <rochel_021459@binalatongan.edu.ph>
2024-06-08 15:36:11 +0800

    Update index.js

--- index.js
+++ index.js
@@ -2,6 +2,10 @@ const btnLike1 = document.getElementById("btnLike1")
 const countLikes1 = document.getElementById("countLikes1")
 const btnLike2 = document.getElementById("btnLike2")
 const countLikes2 = document.getElementById("countLikes2")
+const btnDislike1 = document.getElementById("btnDislike1")
+const countDislike1 = document.getElementById("countDislike1")
+const btnDislike2 = document.getElementById("btnDislike2")
+const countDislike2 = document.getElementById("countDislike2")
 
 const submit = document.getElementById("submit")
 const comment = document.getElementById("comment")
@@ -19,5 +23,14 @@ function clickLike2(){
     let totalLikes2 = parseInt(countLikes2.value) + 1
     countLikes2.textContent = totalLikes2.toString()
 }
+function clickDislike1(){
+   let totalDislike1 = parseInt(countDislike1.value) + 1
+   countDislike1.textContent = totalDislike1.toString() 
+}
+function clickDislike2(){
+   let totalDislike2 = parseInt(countDislike2.value) + 1
+   countDislike2.textContent = totalDislike2.toString() 
+}
 btnLike1.addEventListener("click",clickLike1)
 btnLike2.addEventListener("click",clickLike2)
+
